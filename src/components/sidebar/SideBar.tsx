571688c75a18c285e38424a4f4124af4a254d5ba
Daichi Sato <satodai370054@gmail.com>
2023-06-06 00:38:27 +0900

    チャンネル一覧をAPIから取得・表示

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,26 +1,80 @@
-import React from "react";
 import styled from "styled-components";
 import { ColorConsts } from "../../consts/colorConsts";
 import SideName from "./SideName";
+import { Workspace } from "../WorkspaceNameArea";
+import { useEffect, useState } from "react";
 
-const SideBar = () => {
+type Channel = {
+  id: string;
+  name: string;
+  description: string;
+  createUserId: string;
+  createdAt: string;
+  updatedAt: string;
+  workspaceId: string;
+};
+
+type Props = {
+  workspace: Workspace | undefined;
+};
+
+const SideBar = (props: Props) => {
+  const BACKEND_URL = "http://localhost:8000";
+  const [channels, setChannels] = useState<Channel[]>([]);
+
+  // workspace_idからchannelsを取得する
+  const fetchChannels = async () => {
+    try {
+      const res = await fetch(
+        BACKEND_URL + `/channels?workspace=${props.workspace?.id}`
+      );
+      if (!res.ok) {
+        throw Error(`failed to fetch channels : ${res.status}`);
+      }
+      const channels = await res.json();
+      setChannels(channels);
+      console.log("fetchChannels", channels);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchChannels();
+  }, []);
+
   return (
-    <SideNameListContainer>
-      <SideNameListSubContainer>
-        <SideName name="チャンネル名" hover={false} />
-        <SideName name="チャンネル1" hover={true} />
-        <SideName name="チャンネル2" hover={true} />
-      </SideNameListSubContainer>
-      <SideNameListSubContainer>
-        <SideName name="DM" hover={false} />
-        <SideName name="伊藤博文" hover={true} />
-        <SideName name="藤原業平" hover={true} />
-      </SideNameListSubContainer>
-    </SideNameListContainer>
+    <SideBarContainer>
+      {props.workspace && (
+        <SideNameListContainer>
+          <SideNameListSubContainer>
+            <SideName name="チャンネル" hover={false} />
+            {channels
+              ? channels.map((channel) => (
+                  <SideName key={channel.id} name={channel.name} hover={true} />
+                ))
+              : null}
+          </SideNameListSubContainer>
+          <SideNameListSubContainer>
+            <SideName name="DM" hover={false} />
+            <SideName name="伊藤博文" hover={true} />
+            <SideName name="藤原業平" hover={true} />
+          </SideNameListSubContainer>
+        </SideNameListContainer>
+      )}
+    </SideBarContainer>
   );
 };
 
 export default SideBar;
+
+const SideBarContainer = styled.div`
+  grid-column: 1 / 2;
+  grid-row: 3 / 4;
+  display: flex;
+  flex-direction: column;
+  background-color: ${ColorConsts.ColorTheme.background};
+`;
 
 // 左端に表示するサイドバー
 // 上部にはChannelsContainerを表示する
