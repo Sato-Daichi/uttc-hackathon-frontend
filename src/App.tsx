f4c2d26261de4c972e9c29fe6705a565258b261c
Daichi Sato <satodai370054@gmail.com>
2023-06-10 00:44:55 +0900

    ログインとサインインのルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        {/* <Route path="/create" element={<CreatePost isAuth={isAuth} />}></Route>
-        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/signup/" element={<SignUp />} />
+        <Route path="/login/" element={<LogIn />} />
       </Routes>
     </Router>
   );
